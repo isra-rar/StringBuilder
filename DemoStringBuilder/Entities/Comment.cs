5335d7791b43c6ebacefcbca60e630eee1b7e6da
Israel Araujo <rodg.isra@gmail.com>
2020-08-24 12:26:55 -0300

    Add project files.

---
+++ DemoStringBuilder/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoStringBuilder.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
